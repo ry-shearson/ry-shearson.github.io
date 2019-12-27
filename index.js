49bd448e8053714d0c0ddf8987522b8d2a332906
ry-shears <ryanshearson@gmail.com>
2019-12-26 19:43:54 -0700

    Fixed Exp! Only need to fix spacing between the wording due to the image

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var front = document.querySelectorAll('.front');
 var back = document.querySelectorAll('.back');
-var flipper = document.querySelectorAll('.flipper');
-var exp = document.querySelectorAll('.exp');
+var flipper = document.querySelectorAll('.flip-container');
 var img = document.querySelectorAll('.exp-img');
 
 
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  for (var i = 0; i < exp.length; i++) {
-    exp[i].classList.toggle('hover');
-  }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   for (var i = 0; i < img.length; i++) {
     img[i].style.display = "none";
   }
   for (var i = 0; i < back.length; i++) {
-    back[i].style.transform = "rotateY360deg";
+    back[i].style.transform = "rotateY(360deg)";
     back[i].style.backfaceVisibility = "visable";
+  }
+  for (var i = 0; i < flipper.length; i++) {
+    flipper[i].classList.add('noHover')
   }
 }
