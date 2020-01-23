07101deefa0b47f3a865cc5bb9764e301f50c718
ry-shears <ryanshearson@gmail.com>
2020-01-23 14:35:29 -0800

    removing files no longer in use colorgame back into its own file attempting to fix mobile spacing for exp

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var img = document.querySelectorAll('.exp-img');
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   for (var i = 0; i < img.length; i++) {
     img[i].style.display = "none";
+    img[i].style.height = '50px';
   }
   for (var i = 0; i < back.length; i++) {
     back[i].style.transform = "rotateY(360deg)";
