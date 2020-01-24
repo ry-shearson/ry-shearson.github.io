f2d749f7c0f7cac5e5f2c8fb4e730cc20ceee3e8
ry-shears <ryanshearson@gmail.com>
2020-01-23 18:50:37 -0800

    mobile support for experience spacing

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var front = document.querySelectorAll('.front');
 var back = document.querySelectorAll('.back');
 var flipper = document.querySelectorAll('.flip-container');
 var img = document.querySelectorAll('.exp-img');
+var spone = document.querySelectorAll('spone');
+var spone = document.querySelectorAll('sptwo');
 
 
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   for (var i = 0; i < img.length; i++) {
     img[i].style.display = "none";
-    img[i].style.height = '50px';
   }
   for (var i = 0; i < back.length; i++) {
     back[i].style.transform = "rotateY(360deg)";
@@ -15,5 +16,11 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
   }
   for (var i = 0; i < flipper.length; i++) {
     flipper[i].classList.add('noHover')
+  }
+  for (var i = 0; i < spone.length; i++) {
+    spone[i].style.height = '150px'
+  }
+  for (var i = 0; i < sptwo.length; i++) {
+    sptwo[i].style.height = '200px';
   }
 }
